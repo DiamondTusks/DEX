051faaff665e5565eca36cf226f09cb76a04331f
Pedro <90947391+Nomadpedro@users.noreply.github.com>
2021-09-25 00:43:00 +1000

    updating files

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 Moralis.initialize("7CtANU2X4R5bCfiuhzTD578HekoZ611o62sOYyJm"); // Application id from moralis.io
 Moralis.serverURL = "https://lqqq8io3onds.grandmoralis.com:2053/server"; //Server url from moralis.io
 
+async function init() {
+    await Moralis.initPlugins();
+    await Moralis.enable();     
+    const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
+        chain: 'eth', // The blockchain you want to use (eth/bsc/polygon)
+    });
+    console.log(tokens);
+}
+
 async function login() {
     try {
         currentUser = Moralis.User.current();
@@ -10,10 +19,18 @@ async function login() {
     } catch (error) {
         console.log(error);
     }
-
-    function openModal() {
-        document.getElementById("token_modal").style.display = "block";
-    }
 }
+
+function openModal() {
+    document.getElementById("token_modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("token_modal").style.display = "none";
+}
+
+init();
+
+document.getElementById("close_modal").onclick = closeModal;
 document.getElementById("from_token_select").onclick = openModal;
 document.getElementById("login_button").onclick = login;
